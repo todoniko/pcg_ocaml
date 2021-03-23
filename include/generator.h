410cb52d10e29087cc948c48b007410b3f4fd7c9
todoniko <nmtodorov@gmail.com>
2021-03-23 19:28:12 +0200

    [FIX] Macro expansion and dune configuration

--- include/generator.h
+++ include/generator.h
@@ -2,8 +2,8 @@
 #include "output.h"
 
 /* Low Level PCG */
-#define PCG_DEFAULT_MULTIPLIER_64  6364136223846793005ULL
-#define PCG_DEFAULT_INCREMENT_64   1442695040888963407ULL
+#define PCG_DEFAULT_MUL_64 6364136223846793005ULL
+#define PCG_DEFAULT_INC_64 1442695040888963407ULL
 
 typedef struct pcg_state_64 {
   uint64_t state;
@@ -11,8 +11,8 @@ typedef struct pcg_state_64 {
 
 inline void pcg_oneseq_64_step_r(struct pcg_state_64* rng)
 {
-  rng->state = rng->state * PCG_DEFAULT_MULTIPLIER_64
-    + PCG_DEFAULT_INCREMENT_64;
+  rng->state = rng->state * PCG_DEFAULT_MUL_64
+                          + PCG_DEFAULT_INC_64;
 }
 
 /* PCG API */
@@ -26,24 +26,27 @@ inline void pcg_oneseq_64_srandom_r(struct pcg_state_64* rng,
   pcg_oneseq_64_step_r(rng);
 }
 
-#define DE_OUTPUT(SEQ, OP, S, R) \               
-  inline uint ## R ## _t  \     
-  pcg_ ## SEQ ## _ ## S ## _ ## OP ## _ ## R ## _random_r(struct  pcg_state_ ## S ## * rng) \               
-  { \   
-    uint ## S ## _t oldstate = rng->state; \      
-    pcg_ ## SEQ ## _ ## S ## _step_r(rng); \         
-    return pcg_output_ ## OP ## _ ## S ## _ (oldstate); \          
-  } \   
-   \   
-  inline uint ## R ## _t  \     
-  pcg_ ## SEQ ## _ ## S ## _ ## OP ## _ ## R ## _boundedrand_r(struct pcg_state_ ## S ## * rng, uint ## R ## _t bound) \                 
-  { \   
-    uint ## R ## _t threshold = -bound % bound; \     
-    for (;;) { \   
-      uint ## R ## _t r = pcg_ ## SEQ ## _ ## S ## _ ## OP ## _  ## R ## _r andom_r(rng); \                 
-      if (r >= threshold) \   
-        return r % bound; \   
-    } \   
-  }  
+#define DE_OUTPUT(SEQ, OP, S, R)                           \
+inline uint ## R ## _t                                     \
+pcg_ ## SEQ ## _ ## S ## _ ## OP ## _ ## R ## _random_r(struct  pcg_state_ ## S * rng) \
+{                                                          \
+  uint ## S ## _t oldstate = rng->state;                   \
+  pcg_ ## SEQ ## _ ## S ## _step_r(rng);                   \
+  return pcg_output_ ## OP ## _ ## S ## _ ## R (oldstate); \
+}                                                          \
+                                                           \
+inline uint ## R ## _t                                     \
+pcg_ ## SEQ ## _ ## S ## _ ## OP ## _ ## R ## _boundedrand_r(struct pcg_state_ ## S * rng, uint ## R ## _t bound)             \
+{                                                          \
+  uint ## R ## _t threshold = -bound % bound;              \
+  for (;;) {                                               \
+    uint ## R ## _t r = pcg_ ## SEQ ## _ ## S ## _ ## OP ## _ ## R ## _random_r(rng);                                         \
+    if (r >= threshold) return r % bound;                  \
+  }                                                        \
+}  
 
 DE_OUTPUT(oneseq, xsh_rs, 64, 32)
+DE_OUTPUT(oneseq, xsh_rr, 64, 32)
+DE_OUTPUT(oneseq, rxs_m,  64, 32)
+DE_OUTPUT(oneseq, xsl_rr, 64, 32)
+// DE_OUTPUT(oneseq, xsl_rr_rr, 64, 64)
--- src/pcg_stubs.c
+++ src/pcg_stubs.c
@@ -5,13 +5,13 @@
 #define Val_uint(t) ((Val_int((uint64_t)t)))
 #define Uint_val(V) ((uint64_t)(Int_val(V)))
  
-#define DE_CAML(SEQ, OP, S, R) \
-CAMLprim value \
-SEQ ##_## OP(value unit) \
-{ return Val_uint(pcg_## SEQ ##_## S ##_## OP ##_## R ##_random_r(& SEQ)); } \
-  \
-CAMLprim value \
-SEQ ##_## OP ##_bounded(value b) \
+#define DE_CAML(SEQ, OP, S, R)                         \
+CAMLprim value                                         \
+SEQ ##_## OP(value unit)                               \
+{ return Val_uint(pcg_## SEQ ##_## S ##_## OP ##_## R ##_random_r(& SEQ)); }\
+                                                       \
+CAMLprim value                                         \
+SEQ ##_## OP ##_bounded(value b)                       \
 { return Val_uint(pcg_## SEQ ##_## S ##_## OP ##_## R ##_boundedrand_r(& SEQ, Uint_val(b))); }
 
 prng oneseq;
@@ -23,3 +23,7 @@ oneseq_seed(value s)
 }
 
 DE_CAML(oneseq, xsh_rs, 64, 32)
+DE_CAML(oneseq, xsh_rr, 64, 32)
+DE_CAML(oneseq, rxs_m,  64, 32)
+DE_CAML(oneseq, xsl_rr, 64, 32)
+// DE_CAML(oneseq, xsl_rr_rr, 64, 64)
