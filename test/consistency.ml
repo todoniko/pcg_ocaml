51514f6d248535d83025c5d8587d67d493a8879a
todoniko <nmtodorov@gmail.com>
2021-03-23 16:02:36 +0200

    [ADD] Consistency test for output function `xsh_xs`

---
+++ test/consistency.ml
@@ -0,0 +1,8 @@
+let test_output ~f ~seed ~name =
+  Pcg.seed seed;
+  Printf.printf "# Few results of Pcg.%s with seed 0x%X\n" name seed;
+  for i = 1 to 7 do
+    Printf.printf "%i - 0x%016X\n" i (f ())
+  done
+
+let _ = test_output ~f:Pcg.xsh_xs ~seed:42 ~name:"xsh_xs"
